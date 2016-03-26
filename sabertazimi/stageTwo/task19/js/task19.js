ebb136f853edc7337db437a72230ba62a1e9c1df
sabertazimi <sabertazimi@gmail.com>
2016-03-26 11:01:50 +0800

    Add(Task 19): 完成任务 19
    排序算法的可视化演示

--- sabertazimi/stageTwo/task19/js/task19.js
+++ sabertazimi/stageTwo/task19/js/task19.js
@@ -1,13 +1,9 @@
 (function () {
-    var data = [30, 20 , 30, 40, 10, 30, 70, 40, 80, 15, 20, 47, 88, 100],
+    var data = [30, 20 , 40, 20, 50, 100, 110, 77, 37, 95, 94, 29, 45, 63, 96, 49, 94, 23, 56, 74, 64, 43, 56, 23, 64, 74, 14, 43, 63, 13, 63, 75, 32, 87, 45, 75, 39, 50, 10, 96, 45, 35, 64, 53, 65, 12, 34, 86, 48, 35, 52, 14, 54, 63, 91, 30, 40, 10, 30, 70, 40, 80, 15, 20, 47, 88, 100],
         dom = {
             input: document.getElementById('elem'),
             demo: document.getElementById('demo'),
             // convert to event-delegate
-            // shift: document.getElementById('shift'),
-            // unshift: document.getElementById('unshift'),
-            // pop: document.getElementById('pop'),
-            // push: document.getElementById('push')
             btn: document.getElementById('btn-group')
         },
         utils = {
@@ -51,24 +47,24 @@
         },
         // event delegate mechanism
         bindBtnHandle = function bindBtnHandle() {
-            function spaceCheck() {
+            function removeCheck() {
                 if (data.length <= 0) {
                     alert('元素不足');
-                    return false;
-                } else if (data.length >= 60) {
-                    alert('元素过多');
                     return false;
                 } else {
                     return true;
                 }
             }
 
-            function dataCheck(value) {
+            function insertCheck(value) {
                 if (isNaN(value)) {
                     alert('请输入正整数');
                     return false;
                 } else if (value < 10 || value > 100) {
                     alert('超出范围');
+                    return false;
+                } else if (data.length >= 60) {
+                    alert('元素过多');
                     return false;
                 } else {
                     return true;
@@ -84,97 +80,54 @@
 
                 switch (target.id) {
                     case 'shift':
-                        if (spaceCheck()) {
+                        if (removeCheck()) {
                             data.shift();
                             templateRender(0);
                         }
                         break;
                     case 'unshift':
                         value = utils.toNum(dom.input.value);
-                        if (dataCheck(value)) {
+                        if (insertCheck(value)) {
                             data.unshift(value);
                             templateRender(0);
                         }
                         break;
                     case 'pop':
-                        if (spaceCheck()) {
+                        if (removeCheck()) {
                             data.pop();
                             templateRender(0);
                         }
                         break;
                     case 'push':
                         value = utils.toNum(dom.input.value);
-                        if (dataCheck(value)) {
+                        if (insertCheck(value)) {
                             data.push(value);
                             templateRender(0);
                         }
                         break;
                     case 'sort':
-                        for (var i = 0;i < len;i++) {
-                            for (var j = i + 1; j < len-1;j++) {
+                        var i = 0;
+                        var outeranimation = setInterval(function () {
+                            if (i >= len) {
+                                clearInterval(outeranimation);
+                                return;
+                            }
+
+                            for (var j = i + 1; j < len;j++) {
                                 if (data[j] < data[i]) {
                                     tmp = data[i];
                                     data[i] = data[j];
                                     data[j] = tmp;
-                                    templateRender(i);
-                                    templateRender(j);
                                 }
                             }
-                        }
+                            templateRender(i);
+                            i++;
+                        }, 300);
                         break;
                     default:
                         break;
                 }
             };
-            // dom.shift.addEventListener('click', function () {
-            //     if (data.length <= 0) {
-            //         alert('元素不足');
-            //         return;
-            //     } else if (data.length >= 60) {
-            //         alert('元素过多');
-            //         return;
-            //     }
-            //     data.shift();
-            //     templateRender(data);
-            // }, false);
-            //
-            // dom.unshift.addEventListener('click', function () {
-            //     var value = utils.toNum(dom.input.value);
-            //     if (isNaN(value)) {
-            //         alert('请输入正整数');
-            //         return;
-            //     } else if (value < 10 || value > 100) {
-            //         alert('超出范围');
-            //         return;
-            //     }
-            //     data.unshift(value);
-            //     templateRender(data);
-            // }, false);
-            //
-            // dom.pop.addEventListener('click', function () {
-            //     if (data.length <= 0) {
-            //         alert('元素不足');
-            //         return;
-            //     } else if (data.length >= 60) {
-            //         alert('元素过多');
-            //         return;
-            //     }
-            //     data.pop();
-            //     templateRender(data);
-            // }, false);
-            //
-            // dom.push.addEventListener('click', function () {
-            //     var value = utils.toNum(dom.input.value);
-            //     if (isNaN(value)) {
-            //         alert('请输入正整数');
-            //         return;
-            //     } else if (value < 10 || value > 100) {
-            //         alert('超出范围');
-            //         return;
-            //     }
-            //     data.push(value);
-            //     templateRender(data);
-            // }, false);
         };
 
     (function () {
